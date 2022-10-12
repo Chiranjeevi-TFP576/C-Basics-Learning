5bc2b3d8cdfc9ad750fdd8896f50be29ff923062
Chiranjeevi-TFP576 <p.chiranjeevi12345@gmail.com>
2022-10-12 11:06:50 +0530

    C# UseCases4to8

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using HelloWorld;
+
+//Console.WriteLine("Hello World!");
+Employee obj = new Employee();
+
+//obj.ShowEmployee();
+
+//EmplyoeeOT.Salaries();
+//UC5_CalculateWageFor20DaysInMonth.SalaryFor20DaysInMonth();
+//UC3_PartTimeStatement.DayWages();
+UC4_SwitchStatement.EmployeeWagesUsingSwitch();
+//UC6_CalculateWage100Hrs_or_20Is_Reached.EmpSalaryFor100Hrs();
+//UC7_ComputeEmpWageUsingClassMethod.ComputeSalaryUsingClassMethod();
+//UC8_ComputeEmpWageForMultipleCompanies.ComputeEmpWage("DMart", 20, 2, 10);
+//UC8_ComputeEmpWageForMultipleCompanies.ComputeEmpWage("Amazon", 10, 4, 20);
+UC9EachCompanywage dmart = new UC9EachCompanywage("Dmart", 20, 2, 30);
+dmart.ComputeEmpWage();
+Console.WriteLine(dmart.toString());
